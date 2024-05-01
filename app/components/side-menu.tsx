ca643521095b6ac4d27bfa06ac65121cda49f0c9
Arison Firmino <arisonfirmino.dev@gmail.com>
2024-05-01 18:11:38 -0300

    chore: add a logo to the project and change the favicon

--- app/components/side-menu.tsx
+++ app/components/side-menu.tsx
@@ -3,6 +3,7 @@ import Search from "./search";
 import Nav from "./nav";
 import Form from "./form";
 import { Task } from "../(home)/page";
+import Image from "next/image";
 
 interface SideMenuProps {
   addTask: (newTask: Task) => void;
@@ -12,7 +13,13 @@ export default function SideMenu({ addTask }: SideMenuProps) {
   return (
     <div className="flex h-fit w-full flex-col gap-5 p-5 shadow-md xl:h-full xl:min-w-80 xl:max-w-80 xl:shadow-2xl">
       <div className="flex items-center gap-2.5 text-blue-500">
-        <ListTodoIcon size={20} />
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          height={315}
+          width={315}
+          className="w-7"
+        />
         <h3 className="text-xl font-medium">TaskHub</h3>
       </div>
 
